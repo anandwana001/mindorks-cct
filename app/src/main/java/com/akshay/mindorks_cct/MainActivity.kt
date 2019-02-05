761e6fed7d8c49d69e3f3f74588117b7b69419c2
anandwana001 <akshaynandwana001@gmail.com>
2019-02-05 17:22:12 +0530

    setup androidx browser

---
+++ app/src/main/java/com/akshay/mindorks_cct/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.akshay.mindorks_cct
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val MINDORKS_PUBLICATION = "https://blog.mindorks.com"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+        }
+    }
+}
